124c3fb2b72e1c33793ba9ff57e17c1c10a000e7
ADITYA <aditya11surve08@gmail.com>
2024-09-16 15:03:28 +0530

    final set footer

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ df = pd.read_csv("topics (1).csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # set header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(111, 8, 201)
     pdf.cell(w=0, h=11, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
 
     # set footer
-    pdf.ln(242)
+    pdf.ln(244)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=11, txt=row["Topic"], align="R")
@@ -23,6 +24,7 @@ for index, row in df.iterrows():
         pdf.add_page()
 
         # set footer
+        pdf.ln(255)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=11, txt=row["Topic"], align="R")
