74597284367d1823b7767aa9c6333aa8d6cf7cfc
ADITYA <aditya11surve08@gmail.com>
2024-09-16 14:31:55 +0530

    pdf app - generate multiple pages using for loop

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation= "P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics (1).csv")
 
-pdf.set_font(family="Times", style="B", size=11)
-pdf.cell(w=0, h=11, txt="Aditya Surve", align="L", ln=1)
+for index, row in df.iterrows():
+    pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=11)
-pdf.cell(w=0, h=11, txt="Aditya Surve", align="R", ln=1)
+    pdf.set_font(family="Times", style="B", size=11)
+    pdf.cell(w=0, h=11, txt="Aditya Surve", align="L", ln=1)
 
 pdf.output("output.pdf")
